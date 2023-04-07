aaa5aad50d2ab5f3623040540df3475d5bd79084
Jakub Lhoták <102306929+j-lhotak@users.noreply.github.com>
2023-04-07 15:00:28 +0200

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,13 +1,13 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://us-south.functions.appdomain.cloud/api/v1/web/36230a04-04a3-450d-b0d4-92ff293cf2a7/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
     return $.ajax({
       type: 'GET',
-      url: `${apiUrl}/entries`,
+      url: `/read-guestbook-entries-sequence.json/entries`,
       dataType: 'json'
     });
   },
@@ -16,7 +16,7 @@ const guestbook = {
     console.log('Sending', name, email, comment)
     return $.ajax({
       type: 'PUT',
-      url: `${apiUrl}/entries`,
+      url: `/save-guestbook-entry-sequence.json/entries`,
       contentType: 'application/json; charset=utf-8',
       data: JSON.stringify({
         name,
